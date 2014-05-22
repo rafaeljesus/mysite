bfe96f8a7e14550969d2b3d5cd7824fbe6fa0477
rafaeljesus <rafael_jesus86@hotmail.com>
2014-05-21 22:40:08 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express')
+, load = require('express-load')
+, path = require('path')
+, logfmt = require('logfmt')
+, favicon = require('static-favicon')
+, cookieParser = require('cookie-parser')
+, bodyParser = require('body-parser')
+, app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logfmt.requestLogger());
+app.use(favicon());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+load('controllers')
+  .then('routes')
+  .into(app);
+
+var port = Number(process.env.PORT || 3000);
+
+app.listen(port, function(){
+  console.log('running Rafael Jesus Web Site on port ' + port);
+})
+
+module.exports = app;
