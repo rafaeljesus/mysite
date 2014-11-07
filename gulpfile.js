7a9100a47a78c979b99bad892948846223622f2c
Rafael Jesus <rafael_jesus86@hotmail.com>
2014-11-07 20:38:15 -0200

    update skills

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp')
-, concat = require('gulp-concat')
-, uglify = require('gulp-uglify')
-, minifyCSS = require('gulp-minify-css');
+  , concat = require('gulp-concat')
+  , uglify = require('gulp-uglify')
+  , minifyCSS = require('gulp-minify-css');
 
 var scripts = [
   'public/common-files/js/jquery-1.10.2.min.js'
@@ -15,9 +15,9 @@ var scripts = [
 
 gulp.task('scripts', function() {
   return gulp.src(scripts)
-    .pipe(uglify())
-    .pipe(concat('app.min.js'))
-    .pipe(gulp.dest('public/build/js'));
+  .pipe(uglify())
+  .pipe(concat('app.min.js'))
+  .pipe(gulp.dest('public/build/js'));
 });
 
 var styles = [
@@ -29,9 +29,9 @@ var styles = [
 
 gulp.task('styles', function() {
   gulp.src(styles)
-    .pipe(minifyCSS({ keepBreaks:true }))
-    .pipe(concat('app.min.css'))
-    .pipe(gulp.dest('public/build/css'))
+  .pipe(minifyCSS({ keepBreaks:true }))
+  .pipe(concat('app.min.css'))
+  .pipe(gulp.dest('public/build/css'))
 });
 
 gulp.task('default', ['scripts', 'styles']);
